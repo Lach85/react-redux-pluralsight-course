4f8d175b0a8cf8aff1605c7049dd3e686449e720
DESKTOP-CCJN3VF\racha <rachaelroserossiter@gmail.com>
2018-07-09 17:55:21 +1200

    up to module redux flux - instanciate store and provider

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
-//console.log('Hi');
+import 'babel-polyfill';
+import React from 'react';
+import {render} from 'react-dom';
+import {Router, browserHistory} from 'react-router';
+import configureStore from '../src/store/configureStore';
+import routes from './routes';
+import './styles/styles.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+
+const store = configureStore();
+// redux flux - instanciate store and provider
+
+render (
+    <Router history={browserHistory} routes={routes} />,
+    document.getElementById('app')
+);
---
+++ src/routes.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Route, IndexRoute } from 'react-router';
+import App from '../src/components/App';
+import HomePage from '../src/components/home/HomePage';
+import AboutPage from '../src/components/about/AboutPage';
+import CoursePage from '../src/components/courses/CoursesPage';
+
+export default (
+  <Route path="/" component={App}>
+    <IndexRoute component={HomePage} />
+    <Route path="about" component={AboutPage} />
+    <Route path="courses" component={CoursePage} />
+  </Route>
+);
